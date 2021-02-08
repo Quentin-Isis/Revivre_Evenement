f49b3cc6ede084e435ae4e629fba9c69bdb770d0
QUENTIN <QUENTIN@QJ-PC-91>
2021-02-08 09:47:05 +0100

    Initialisation dossiers

---
+++ src/main/java/Revivre_Evenement/WebApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Revivre_Evenement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author QUENTIN
+ */
+@SpringBootApplication
+public class WebApp {
+    
+    public static void main(String[] args) {
+            SpringApplication.run(WebApp.class, args);
+	}
+    
+}
