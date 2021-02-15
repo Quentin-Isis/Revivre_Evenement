2f56cf3d5850ce6bc712c5019eb559157dc49236
mathieu.valette1 <mathieu.valette1@gmail.com>
2021-02-15 12:17:04 +0100

    map incrusté sur l'index

---
+++ src/main/ressources/static/javascript/map.js
@@ -0,0 +1,14 @@
+/**
+ * Integration de la carte sur la page
+ */
+
+
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 43.624227611324216,  lng: 2.268580304110914 },
+    zoom: 15,
+  });
+}
