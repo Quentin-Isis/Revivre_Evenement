e17310c19d117a1bc330a84d905b69bc32798f3e
mathieu.valette1 <mathieu.valette1@gmail.com>
2021-03-16 18:52:42 +0100

    update controller

--- src/main/java/revivreEvenement/controller/EvenementController.java
+++ src/main/java/revivreEvenement/controller/EvenementController.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package revivreEvenement.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 /**
  *
- * @author QUENTIN
+ * @author Mathieu
  */
-public class EvenementController {
+
+@Controller
+@RequestMapping(path="/evenement")
+public class EvenementController{
     
+    @GetMapping("eventRandom")
+    public String showEvenementRandom(Model model){
+        /**
+         * Return la page wiki d'un évènement choisi au hasard
+         * pour l'instant, renvoie juste la page wiki
+         */
+        return "wiki"; /// Provisoire
+    }
+    
+    @GetMapping("contribuer")
+    public String showContribuer(Model model){
+        return "contribuer";
+    }
+    
+    @GetMapping("listeRessourcesEvent")
+    public String showListeRessourcesEvent(Model model){
+        return "liste_ressources_event";
+    }
+    
+    @GetMapping("search")
+    public String showSearchPage(Model model){
+        return "search";
+    }
 }
+    
+    
+
