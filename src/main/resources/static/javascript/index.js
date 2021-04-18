8ec2fe80635a71e5cb7f890ee16993beac04f6b4
Tristan <Tristan@DESKTOP-6VMTE5H>
2021-04-18 12:29:14 +0200

    Boutons Frise fonctionnels

--- src/main/resources/static/javascript/index.js
+++ src/main/resources/static/javascript/index.js
@@ -4,10 +4,37 @@
  * and open the template in the editor.
  */
 
-document.getElementById("date").addEventListener('click', () => {
-    if (getComputedStyle(document.getElementById("liste-evenement")).display != "none") {
-        document.getElementById("liste-evenement").style.display = "none";
-    } else {
-      document.getElementById("liste-evenement").style.display = "block";
+let dates = document.getElementsByClassName("date");
+let listeEvenement = document.getElementsByClassName("liste-evenement");
+
+for(let i = 0; i< dates.length;i++){
+    dates[i].addEventListener('click',affichage);
+}
+
+function searchIndexOf(lienCible){
+    let index = 0;
+    if (lienCible.className == "date"){
+        for (let i=0; i<dates.length;i++){
+            if (lienCible == dates[i]){
+                return index;
+            }
+            else{
+                index+=1;
+            }
+        }
     }
-})
+    }
+    
+function affichage(event){
+    let lienCible = event.target;
+    console.log(lienCible);
+    let index = searchIndexOf(lienCible);
+    let listeUtile = listeEvenement[index];
+    console.log(index);
+    console.log(listeUtile);
+    if (getComputedStyle(listeUtile).display != "none") {
+        listeUtile.style.display = "none";
+    } else {
+      listeUtile.style.display = "block";
+    }
+}
