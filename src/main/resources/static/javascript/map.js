c1b5f12893830807d336f8d00d9d219409562402
VongLea <lea.vong@etud.univ-jfc.fr>
2021-04-21 08:18:31 +0100

    map qui centre sur le bon lieu

--- src/main/resources/static/javascript/map.js
+++ src/main/resources/static/javascript/map.js
@@ -2,37 +2,46 @@
  * Integration de la carte sur la page
  */
 
-//var url = "http://localhost:8080/api";
-var url = "https://lit-plains-26980.herokuapp.com/api"
+var url = "https://lit-plains-26980.herokuapp.com"
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 43.624227611324216,  lng: 2.268580304110914 },
-        zoom: 15,
+        zoom: 5,
     });
 }
 
 function goToPlace() {
+    var adresse = [];
     initMap();
     $.ajax({
-        url : url + "/evenements"
+        url : url + "/api/evenements"
     }).done(function(dataJSON) {
-        for (let j=0; j<dataJSON.content.length; j++) {
-            adresse = dataJSON.content[j].lieu;
-            var geocoder = new google.maps.Geocoder();
-            geocoder.geocode({'address':adresse}, function(results, status) {
-                if (status == 'OK') {
-                    map.setCenter(results[0].geometry.location);
-                }
-            });
-        }
+        $.ajax({
+            url : url + "/api/evenements/"+dataJSON.content[0].id+"/items"
+        }).done(function() {
+            for (let j=0; j<dataJSON.content.length; j++) {
+                adresse[j] = dataJSON.content[j].lieu;
+            }
+            for (let k=0; k<adresse.length; k++) {
+                if (adresse[k] == document.getElementById("adresse").attributes.value.textContent) {
+                    lieu = adresse[k];
+                    const geocoder = new google.maps.Geocoder();
+                    geocoder.geocode({'address':lieu}, function(results, status) {
+                        if (status == 'OK') {
+                            map.setCenter(results[0].geometry.location);
+                        }
+                    });
+                } 
+            }
+        })
     });
     $.ajax({
-        url : url + "/items"
+        url : url + "/api/items"
     }).done(function(dataJson) {
         for (let i=0; i<dataJson.content.length; i++) {
             $.ajax({
-                url : url+"/items/"+dataJson.content[i].id+"/position"
+                url : url+"/api/items/"+dataJson.content[i].id+"/position"
             })
             .done(function(json) {
                 var marker = new google.maps.Marker({
